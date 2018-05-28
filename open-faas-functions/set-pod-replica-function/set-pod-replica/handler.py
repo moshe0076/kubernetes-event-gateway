2bb8215a59920955422f138eb5c99a0010797693
Moshe Nadler <moshen@sweetinn.com>
2018-05-28 16:14:19 +0300

    open-faas

--- open-faas-functions/set-pod-replica-function/set-pod-replica/handler.py
+++ open-faas-functions/set-pod-replica-function/set-pod-replica/handler.py
@@ -1,31 +1,34 @@
 from kubernetes import client, config
 import json
 import traceback
+import os
 
-
-def handle(req):
+def handle(event):
     """handle a request to the function
     Args:
-        req (str): request body
+        event (str): CloudEvent
     """
     try:
-        req = json.loads(req)
+        event=json.loads(event)
+        deploymentName=event['data']['body']['deploymentName']
+        replicaCount=int(event['data']['body']['replicaCount'])
     except:
-        print 'There was an error parsing the handle req "%s": \n' %(req) + str(traceback.format_exc())
+        return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error parsing the event"}'
     try:
-       config.load_incluster_config()
+        config.load_incluster_config()
     except:
-        print 'There was an error loading the Kubernetes config: \n' %(req) + str(traceback.format_exc())
+        return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error loading Kubernetes config"}'
     try:
-        field_selector='metadata.name='+req['deploymentName']
+        field_selector = 'metadata.name='+deploymentName
         api_v1beta1 = client.ExtensionsV1beta1Api()
         spec = client.ExtensionsV1beta1DeploymentSpec(
-            replicas=req['replicaCount'],
+            replicas=replicaCount,
             template=api_v1beta1.list_namespaced_deployment(namespace="operation-dev-infra", field_selector=field_selector))
         body = client.ExtensionsV1beta1Deployment(spec=spec)
-        api_v1beta1.patch_namespaced_deployment_scale(namespace='operation-dev-infra', name=req['deploymentName'], body=body)
-        print '"%s" deployment replica count was set to "%s".' %(req['deploymentName'], req['replicaCount'])
+        api_v1beta1.patch_namespaced_deployment_scale(namespace='operation-dev-infra', name=deploymentName, body=body)
+        return '{"statusCode": 200, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "%s deployment replica count was changed to %d successfully"}' %(deploymentName, replicaCount)
     except:
-        print 'There was an error changing "%s" deployment replica count to "%s" : \n' %(req['deploymentName'], req['replicaCount']) + str(traceback.format_exc())
+        return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error changing deployment replica count"}'
+
 
 
