5b06ad715924696cbcd302ed26a04bdfe831e53b
Moshe Nadler <moshen@sweetinn.com>
2018-06-03 14:24:23 +0300

    open-faas

--- open-faas-functions/set-pod-replica-function/set-pod-replica/handler.py
+++ open-faas-functions/set-pod-replica-function/set-pod-replica/handler.py
@@ -1,7 +1,6 @@
 from kubernetes import client, config
 import json
-import traceback
-import os
+import requests
 
 def handle(event):
     """handle a request to the function
@@ -12,6 +11,8 @@ def handle(event):
         event=json.loads(event)
         deploymentName=event['data']['body']['deploymentName']
         replicaCount=int(event['data']['body']['replicaCount'])
+        kubeEventId=event['eventID']
+        kubeEventTime=event['eventTime']
     except:
         return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error parsing the event"}'
     try:
@@ -23,12 +24,27 @@ def handle(event):
         api_v1beta1 = client.ExtensionsV1beta1Api()
         spec = client.ExtensionsV1beta1DeploymentSpec(
             replicas=replicaCount,
-            template=api_v1beta1.list_namespaced_deployment(namespace="operation-dev-infra", field_selector=field_selector))
+            template=api_v1beta1.list_namespaced_deployment(namespace="test-infra", field_selector=field_selector))
         body = client.ExtensionsV1beta1Deployment(spec=spec)
-        api_v1beta1.patch_namespaced_deployment_scale(namespace='operation-dev-infra', name=deploymentName, body=body)
-        return '{"statusCode": 200, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "%s deployment replica count was changed to %d successfully"}' %(deploymentName, replicaCount)
+        api_v1beta1.patch_namespaced_deployment_scale(namespace='test-infra', name=deploymentName, body=body)
     except:
         return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error changing deployment replica count"}'
+    try:
+        body = {
+            "replicaCount": replicaCount,
+            "deploymentName": deploymentName,
+            "kubeEventId": kubeEventId,
+            "kubeEventTime": kubeEventTime
+        }
+        r = requests.post(
+            'http://event-gateway:4000/aws-lambda/update-dynamodb-kubernetes-deployment-replica-count-table',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(body))
+        if r.status_code > 299:
+            raise Exception
+    except:
+        return '{"statusCode": 500, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "There was an error Updating DynamoDB"}'
+    return '{"statusCode": 200, "headers": {"Content-Type": "text/plain; charset=utf-8"}, "body": "%s deployment replica count was changed to %d successfully"}' %(deploymentName, replicaCount)
 
 
 
